8cd14f593de65e41ee7024782b445e6607065679
badkamyk <slothmateusz@gmail.com>
2023-01-15 17:46:49 +0100

    init commit

---
+++ base/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('login/', views.login_page, name='login'),
+    path('logout/', views.logout_user, name='logout'),
+    path('register/', views.register_page, name='register'),
+    path('', views.home, name='home'),
+    path('room/<str:pk>/', views.room, name='room'),
+    path('profile/<str:pk>', views.user_profile, name='user_profile'),
+    path('create_room/', views.create_room, name='create_room'),
+    path('update_room/<str:pk>', views.update_room, name='update_room'),
+    path('delete_room/<str:pk>', views.delete_room, name='delete_room'),
+    path('delete_message/<str:pk>', views.delete_message, name='delete_message'),
+    path('update-user', views.update_user, name='update_user'),
+    path('topics', views.topics_page, name='topics'),
+    path('activity', views.activity_page, name='activity'),
+
+]
